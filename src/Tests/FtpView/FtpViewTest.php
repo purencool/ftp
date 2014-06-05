1dad93f860c46651739c9e298d2e64cae58cb653
FB <fiona@purencool.com.au>
2014-06-05 18:22:04 +1000

    Tested to see if FtpViewTest to see if it works

--- src/Tests/FtpView/FtpViewTest.php
+++ src/Tests/FtpView/FtpViewTest.php
@@ -1,11 +1,17 @@
 <?php
-
+/**
+ * Test the FtpViewClass
+ */
 namespace FtpView\Tests;
 
 class FtpViewTest extends \PHPUnit_Framework_TestCase
 {
- /**
-  *
-  */
-
+  /**
+   * Test phpunit
+   * @assert (true, true) == true
+   */
+   public function testPhpUnit()
+   {
+    $this->assertEquals('true','true');
+   } 
 }
