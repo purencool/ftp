208dbf6ee585cf6d8f9a62dd1915080407d6fd69
FB <fiona@purencool.com.au>
2014-06-05 19:19:08 +1000

    Added some more fixes

---
+++ src/Tests/FtpView/FtpClientTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Test the FtpViewClass
+ */
+namespace FtpView\Tests;
+
+class FtpClientTest extends \PHPUnit_Framework_TestCase
+{
+  /**
+   * Test phpunit
+   * @assert (true, true) == true
+   */
+   public function testPhpUnit()
+   {
+    $this->assertEquals('true','true');
+   }
+   
+  /**
+   * Test FtpClient __toString()
+   * @assert ($ftpClient->_toString(), 'FtpView\FtpClient') == true
+   */
+   public function testFtpClient()
+   {
+       
+   } 
+}
